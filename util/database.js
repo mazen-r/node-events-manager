e6ab56a68b26f5be29ac01ddb4b350ae2b4aee56
Mazen Ramadan <mazenramadan00@gmail.com>
2022-07-18 14:44:10 +0200

    event model

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-  mongoose.connect('mongodb://localhost:27017/events', (err) => {
+  mongoose.connect('mongodb://localhost:27017/events', {useNewUrlParser: true}, (err) => {
     if (err) {
         console.log(err);
     } else {
