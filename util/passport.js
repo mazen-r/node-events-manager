eaef49f9e82213a309db427b1ed2c071d8ecb15e
Mazen Ramadan <mazenramadan00@gmail.com>
2022-07-20 21:01:19 +0200

    validating user credinatials

--- util/passport.js
+++ util/passport.js
@@ -3,10 +3,44 @@ const localStrategy = require('passport-local').Strategy
 
 const User = require('../models/User')
 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+}); 
+
 passport.use('local.signup', new localStrategy({
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
 }, (req, username, password, done) => {
-    console.log(req.body);
+    if  (req.body.password != req.body.confirm_password) {
+        return done(null, false, req.flash('error', 'passwords do not match'));
+    } else {
+        User.findOne({email: username}, (err, user) => {
+            if(err) {
+                return done(err)
+            };
+            if(user) {
+                return done(null, false, req.flash('error', 'Email is already used'))
+            }
+            if (!user) { //crates a user
+                let newUser = new User();
+                newUser.email = req.body.email;
+                newUser.password = req.hashPassword(req.body.password);
+                newUser.save((err, user) => {
+                    if (!err) {
+                        return done(null, user, req.flash('success', 'User Added'));
+                    } else {
+                        console.log(err);
+                    };
+                });
+            };
+        });
+    };
 }));
+
